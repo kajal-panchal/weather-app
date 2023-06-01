c1aa792e158d64a6f747123c5bf605af424dbcc0
kajal-panchal <kajal.panchal@e.incipientinfo.com>
2023-06-01 19:07:15 +0530

    weather App

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+
+import HomeView from "../views/HomeView.vue";
+
+import CityView from "../views/CityView.vue";
 
 const routes = [
   {
@@ -8,13 +11,12 @@ const routes = [
     component: HomeView
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+
+    path: '/weather/:state/:city',
+    name: 'cityView',
+    component: CityView
   }
+    
 ]
 
 const router = createRouter({
@@ -22,4 +24,15 @@ const router = createRouter({
   routes
 })
 
+
+
+
+router.beforeEach((to, from, next) => {
+  document.title = `${
+    to.params.state
+      ? `${to.params.city}, ${to.params.state}`
+      : to.meta.title
+  } | The Local Weather`;
+  next();
+});
 export default router
