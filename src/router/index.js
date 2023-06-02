4818897cca1e89b3c1cf6f59ed841cba85acabd6
kajal-panchal <kajal.panchal@e.incipientinfo.com>
2023-06-02 10:12:43 +0530

    update changes

--- src/router/index.js
+++ src/router/index.js
@@ -26,13 +26,4 @@ const router = createRouter({
 
 
 
-
-router.beforeEach((to, from, next) => {
-  document.title = `${
-    to.params.state
-      ? `${to.params.city}, ${to.params.state}`
-      : to.meta.title
-  } | The Local Weather`;
-  next();
-});
 export default router
